757e044f0c17d508f0e9b0df5ae8d5a681c24380
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-20 22:35:44 +0530

    Add files via upload
    uc1+uc2+uc3 added of line comparision problem

---
+++ UC01Linelength.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08LineComparision
+{
+    public class UC01Linelength
+    {
+        public void linecompareMethod(Double x1, Double x2, Double y1, Double y2)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("enter end points of line 01 starting point (x1,y1)and ending point (x2,y2) values");
+            Console.WriteLine("Entered start  points are" + (x1, y1) + "And Entered End points are" + (x2, y2));
+            Double Linelength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("linelength is " + Linelength);
+
+        }
+
+    }
+    public class Program1
+    {
+        public static void Main(string[] args)
+        {
+            UC01Linelength prob01 = new UC01Linelength();
+
+            prob01.linecompareMethod(2.2, 3.3, 5.5, 9.9);
+
+        }
+    }
+}
+
---
+++ Usercase02LineCompare.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08LineComparision
+{
+    public class Usercase02LineCompare
+    {
+        public Double line01Method(Double x1, Double x2, Double y1, Double y2, int i)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("enter end points of line " + i + " starting point (x1,y1)and ending point (x2,y2) values");
+            Console.WriteLine("Entered start  points are" + (x1, y1) + "And Entered End points are" + (x2, y2));
+            Double Linelength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("linelength is " + Linelength);
+
+            return +Linelength;
+        }
+
+
+    }
+    public class Program2
+    {
+        public static void Main()
+        {
+            Usercase02LineCompare ObjClsLine01 = new Usercase02LineCompare();
+            Usercase02LineCompare ObjClsLine02 = new Usercase02LineCompare();
+
+            Double line01length;
+            line01length = ObjClsLine01.line01Method(2.2, 3.3, 5.5, 9.9, 1);
+            Double line02length;
+            line02length = ObjClsLine02.line01Method(2.5, 4.3, 5.4, 9.5, 2);
+
+            if (line01length.Equals(line02length))
+
+            {
+                Console.WriteLine("Line01 is equals to line02");
+            }
+            else
+            {
+                Console.WriteLine("Line01 is not equals to line02");
+            }
+
+
+
+
+        }
+    }
+
+}
